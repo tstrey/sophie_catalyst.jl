39ae1f7e70fc5c1b75254421b44bf3583e878645
sophie strey <sophia.strey@gmail.com>
2023-07-05 12:20:22 -0400

    fixed switching reaction model

--- switching_system.jl
+++ switching_system.jl
@@ -8,6 +8,8 @@ using Plots
 @variables t k_on(t)=10.0
 @species A(t) = 10.0 B(t) = 0.0
 @parameters k_off
+
+#try without DSL => rxs = [(@reaction $v, 0 --> A), @reaction 1.0, A --> 0]
 rn = @reaction_network begin
     ($k_on, k_off), A <--> B
 end
@@ -17,18 +19,20 @@ tspan = (0.0, 20.0)
 p = [k_on => 10.0, k_off => 10.0]
 
 u0 = [A => 10.0, B => 0.0, k_on => 10.0]
-continuous_events = [B/A ~ 0.95] => [k_on ~ 0.0]
+discrete_events = [B/A ~ 0.95] => [k_on ~ 0.0]
 
-@named rs = ReactionSystem([rn],t; continuous_events)
-oprob = ODEProblem(rs,u0,tspan,p)
-#sample_times = range(tspan[1]; stop = tspan[2], length = 100)
+@named rs = ReactionSystem([rn],t; discrete_events)
+osys = convert(ODESystem, rs)
+oprob = ODEProblem(rn, u0, tspan, p)
 
 sol = solve(oprob, Tsit5())
-#sample_vals = Array(sol_real(sample_times))
-#sample_vals .*= (1 .+ .1 * rand(Float64, size(sample_vals)) .- .05)
+sample_times = range(tspan[1]; stop = tspan[2], length = 100)
+sample_vals = Array(sol_real(sample_times))
+sample_vals .*= (1 .+ .1 * rand(Float64, size(sample_vals)) .- .05)
 
 default(; lw = 3, framestyle = :box, size = (800, 400))
 plot(sol)
+
 plot(sol_real; legend = nothing, color = [:darkblue :darkred])
 scatter!(sample_times, sample_vals'; color = [:blue :red], legend = nothing)
 
